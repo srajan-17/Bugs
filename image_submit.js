ff90808ff17a7edaba6d2fabc6a01deccfdcc707
Siddharth Rajan <siddharthrajan10@gmail.com>
2018-04-08 11:37:13 -0400

    4/08 11:37am

--- image_submit.js
+++
@@ -1,3 +0,0 @@
-function alertm() {
-  alert("The creature in this image is a Goliath birdeater, or Theraphosa blondi.");
-}
--- image_submit.py
+++
@@ -1,9 +0,0 @@
-from clarifai.rest import ClarifaiApp
-from clarifai.rest import Image as ClImage
-
-app = ClarifaiApp(api_key='c4406989d7624ad0aa336616ea3e4ada')
-
-model = app.models.get('Bugz')
-image = ClImage(url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Theraphosa_blondi_MHNT.jpg/220px-Theraphosa_blondi_MHNT.jpg')
-model.predict([image])
-print model.predict_by_url(url='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Theraphosa_blondi_MHNT.jpg/220px-Theraphosa_blondi_MHNT.jpg')
