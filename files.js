3c92e2f11e13e0f240ce5a1b2c880b875d6d3e83
Siddharth Rajan <siddharthrajan10@gmail.com>
2018-04-07 23:52:39 -0400

    4/07 11:52pm

--- files.js
+++ files.js
@@ -1,6 +1,24 @@
 function upload() {
-	var imgcanvas = document.getElementById("can");
+	var imgcanvas = document.getElementById("canvas");
 	var fileinput = document.getElementById("finput");
+	//var image = new SimpleImage(fileinput);
+	//image.drawTo(imgcanvas);
 	//console.log(imgcanvas);
 	console.log(fileinput.value);
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var img = document.getElementById(fileinput.value);
+  ctx.drawImage(img, 10, 10);
 }
+
+function readURL(input) {
+			 if (input.files && input.files[0]) {
+					 var reader = new FileReader();
+					 reader.onload = function (e) {
+							 $('#blah').attr('src', e.target.result);
+					 };
+					 reader.readAsDataURL(input.files[0]);
+			 }
+			 var fileinput = document.getElementById("finput");
+			 console.log(fileinput.value + " has been uploaded");
+	 }
